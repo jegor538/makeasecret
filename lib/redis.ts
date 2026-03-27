d3843f16a71042d3d776dea47fb16a6d72161e1a
jegor538 <jegor538@proton.me>
2026-03-27 23:47:33 +0100

    Update redis.ts

--- lib/redis.ts
+++ lib/redis.ts
@@ -3,11 +3,12 @@ import { Redis } from "ioredis";
 const UPSTASH_REDIS_REST_URL = "https://bright-maggot-86346.upstash.io";
 const UPSTASH_REDIS_REST_TOKEN = "AYXAAIncDI4ZjI0NTUxNDUyOWY0NWE5YjdmNmY1MmE1OGU4ODIwY3AyODYzNDY";
 
-const getRedisUrl = () => {
-  return `redis://default:${UPSTASH_REDIS_REST_TOKEN}@${UPSTASH_REDIS_REST_URL.replace("https://", "").replace(":443", "")}`;
-};
-
-export const redis = new Redis(getRedisUrl());
+const redis = new Redis({
+  host: UPSTASH_REDIS_REST_URL.replace("https://", "").split(":")[0],
+  port: 6379,
+  password: UPSTASH_REDIS_REST_TOKEN,
+  tls: {},
+});
 
 export const saveSecret = async (id: string, text: string, password: string | null) => {
   const data = JSON.stringify({ text, password });
